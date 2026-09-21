5aa96f78f8a0ed6454fdddde193f0120f82b378a
EniolaJonathanDare-S <G00476368@atu.ie>
2026-09-21 12:52:36 +0100

    added displayDetails method to Book

--- src/main/java/ie/atu/oop/week1/Book.java
+++ src/main/java/ie/atu/oop/week1/Book.java
@@ -8,9 +8,9 @@ public class Book {
     public boolean available= true;
 
     public void displayDetails(){
-        System.out.println("First book title: " + title);
-        System.out.println("First book author: " + author);
-        System.out.println("First book page count: "+ pageCount);
-        System.out.println("First book available: " + available);
+        System.out.println("Book title: " + title);
+        System.out.println("Book author: " + author);
+        System.out.println("Book page count: "+ pageCount);
+        System.out.println("Book available: " + available);
     }
 }
